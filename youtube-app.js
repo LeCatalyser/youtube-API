4960544583cba69b51ab38b44bf052824a9d4d98
Luisa Salcedo <profe.salcedo@gmail.com>
2017-04-17 20:04:47 -0500

    added string interpolation and array to print multiple thumbnails

--- youtube-app.js
+++ youtube-app.js
@@ -1,15 +1,36 @@
 
-var OMDB_BASE_URL = 'https://www.omdbapi.com/';
+var youtube_base_URL = 'https://www.googleapis.com/youtube/v3/search';
+
 
 $(function(){
 	$("form").submit(function(event){
+		event.preventDefault();
 		var input = $(".search").val();
 		var query = {
-		    s: input,
-		    r: 'json'
+		    q: input,
+		    key: "AIzaSyBEuPnd9L3XtxsWUXEjqbBHKpuoz0qITh0",
+		    part: "snippet",
+		    type: "video",
+		  
 		}
-		$.getJSON(OMDB_BASE_URL, query, function(data) {
-			console.log(data);
+		$.getJSON(youtube_base_URL, query, function(data) {
+			renderData(data, input);
 		});
 	});
+
 });
+
+var renderData = function(data, input) {
+	console.log(data);
+	// $('.results').html(data.items[0].snippet.thumbnails.default.url);
+	
+	var items = data.items.map(function(item) {
+		return 	`
+			<img src ="${item.snippet.thumbnails.default.url}">
+		`
+//add title, 
+	});
+	$('.results').html(`
+		${items.join("")}
+	`)
+}
